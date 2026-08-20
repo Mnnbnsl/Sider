7cd6a59cb305b4f0a2298c4f4978288dfb78abfe
Manan Bansal <manan.bansal0302@gmail.com>
2026-08-20 19:25:46 +0000

    Added encoder in separate file

--- internal/resp/decoder.go
+++ internal/resp/decoder.go
@@ -3,7 +3,6 @@ package resp
 import (
 	"errors"
 	"strconv"
-	"fmt"
 ) 
 
 // Implementing RESP2 for now.
@@ -253,34 +252,3 @@ func Decode(data []byte) (interface{}, error) {
 	return value, err
 }
 
-func Encode(value interface{}, isSimple bool) []byte {
-	switch v := value.(type) {
-
-	case string:
-		if isSimple {
-			return []byte(fmt.Sprintf("+%s\r\n", v))
-		}
-
-		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
-
-	case int:
-		return []byte(fmt.Sprintf(":%d\r\n", v))
-
-	case []interface{}:
-		var result []byte
-
-		result = append(result, fmt.Sprintf("*%d\r\n", len(v))...)
-
-		for _, item := range v {
-			result = append(result, Encode(item, false)...)
-		}
-
-		return result
-
-	case nil:
-		// Null bulk string
-		return []byte("$-1\r\n")
-	}
-
-	return []byte{}
-}
--- internal/resp/encoder.go
+++ internal/resp/encoder.go
@@ -1,2 +1,35 @@
 package resp
 
+import "fmt"
+
+func Encode(value interface{}, isSimple bool) []byte {
+	switch v := value.(type) {
+
+	case string:
+		if isSimple {
+			return []byte(fmt.Sprintf("+%s\r\n", v))
+		}
+
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+
+	case int:
+		return []byte(fmt.Sprintf(":%d\r\n", v))
+
+	case []interface{}:
+		var result []byte
+
+		result = append(result, fmt.Sprintf("*%d\r\n", len(v))...)
+
+		for _, item := range v {
+			result = append(result, Encode(item, false)...)
+		}
+
+		return result
+
+	case nil:
+		// Null bulk string
+		return []byte("$-1\r\n")
+	}
+
+	return []byte{}
+}
